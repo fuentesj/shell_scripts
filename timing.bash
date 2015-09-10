a6abee075a7bcb0a44b2bd064660feba993f7e4e
jonathan <jonathan.wilfredo.fuentes@gmail.com>
2015-09-10 07:45:54 -0700

    Updated timing bash script to show running time for each individual HTTP request. Need to figure out how to convert strings into floating point numbers.

--- timing.bash
+++ timing.bash
@@ -4,9 +4,16 @@ read URL
 echo "Please enter in the number of times this URL should be accessed:"
 read NUM_OF_ACCESSES
 
+REGEX="([0-9]{1,3}\.[0-9]{3})"
+RUNNING_TOTAL=0
+
 
 echo "Now accessing " $URL $NUM_OF_ACCESSES " times."
 for (( iteration=1; iteration <= $NUM_OF_ACCESSES; iteration++))
 do
-	{ time curl $URL  > /dev/null; } |& grep real
+	CURRENT_TIME=$(curl -s -w "%{time_total}\n" $URL -o /dev/null)
+	echo "Current Request Time: " $CURRENT_TIME "s"
+	#RUNNING_TOTAL=$((RUNNING_TOTAL+=CURRENT_TIME))
 done
+
+echo $RUNNING_TOTAL
