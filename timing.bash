94d6a7162ed46d3f5649cb23a9abb4b38de255ca
jonathan <jonathan.wilfredo.fuentes@gmail.com>
2015-09-11 17:44:18 -0700

    Added bash shebang Removed unused regex Fixed small formatting issue

--- timing.bash
+++ timing.bash
@@ -1,22 +1,20 @@
+#!/bin/bash
+
 echo "Please enter a URL: "
 read URL
 
 echo "Please enter in the number of times this URL should be accessed:"
 read NUM_OF_REQUESTS
 
-REGEX="([0-9]{1,3}\.[0-9]{3})"
 RUNNING_TOTAL=0
-
-
 echo "Now accessing" $URL $NUM_OF_REQUESTS "times."
-for (( iteration=1; iteration <= $NUM_OF_REQUESTS; iteration++))
+for (( iteration=1; iteration <= $NUM_OF_REQUESTS; iteration++ ))
 do
 	CURRENT_TIME_STRING=$(curl -s -w "%{time_total}\n" $URL -o /dev/null)
 	echo "Current Request Time: " $CURRENT_TIME_STRING "s"
 	CURRENT_TIME_NUM=$(bc <<< "scale=3;$CURRENT_TIME_STRING")
 	RUNNING_TOTAL=$(echo "scale=3;$RUNNING_TOTAL + $CURRENT_TIME_NUM;" | bc)
 done
-
 
 MEAN_RUNNING_TIME=$(echo "scale=3;$RUNNING_TOTAL / $NUM_OF_REQUESTS;" | bc)
 echo "Mean Request Time:" $MEAN_RUNNING_TIME "s"
